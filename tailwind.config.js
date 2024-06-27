6ae43b99b6d358c30556fe51ef02a1e704c3ed10
react4.smart <react.4@smartgate-egypt.com>
2024-06-27 15:47:29 +0200

    last update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      // tablet: "640px",
+      // => @media (min-width: 640px) { ... }
+      // laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+      // desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {},
   },
   plugins: [],
 };
+
+// Need To install Prettier For Tailwind
